3d15906f5a13b4b696804304c4fdc9eb67ffdbea
XXN <xu>
2024-12-03 23:21:44 +0800

    delete the test

--- src/test/java/com/xuxunne/usercenter/SampleTest.java
+++ src/test/java/com/xuxunne/usercenter/SampleTest.java
@@ -1,3 +1,4 @@
+/*
 package com.xuxunne.usercenter;
 
 import com.xuxunne.usercenter.pojo.User;
@@ -7,21 +8,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import java.util.List;
-//
-//@SpringBootTest
-//public class SampleTest {
-//
-//    @Resource//默认先按照JavaBean的名称进行注入
-//    //   @Autowired 只会按照对象类型去注入
-//
-//    private UserMapper userMapper;
-//
-//    @Test
-//    public void testSelect() {
-//        System.out.println(("----- selectAll method test ------"));
-//        List<User> userList = userMapper.selectList(null);
-//        Assert.isTrue(5 == userList.size(), "");
-//        userList.forEach(System.out::println);
-//    }
-//
-//}
+
+@SpringBootTest
+public class SampleTest {
+
+    @Resource//默认先按照JavaBean的名称进行注入
+    //   @Autowired 只会按照对象类型去注入
+
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.isTrue(5 == userList.size(), "");
+        userList.forEach(System.out::println);
+    }
+
+}*/
